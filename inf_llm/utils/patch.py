f919b87557571aa0c7517173d221a42702152d0e
Nithid Anchaleenukoon <nithidan@mit.edu>
2025-11-19 16:39:14 -0500

    fix(infllm.utils.patch): resolve attribute missing self.num_heads

--- inf_llm/utils/patch.py
+++ inf_llm/utils/patch.py
@@ -19,12 +19,17 @@ def huggingface_forward(forward):
         **kwargs,
     ):
         assert not output_attentions
+        try:
+          num_heads = self.num_heads 
+        except AttributeError:
+          num_heads = self.num_attention_heads
         ret = forward(
             self, hidden_states, hidden_states,
             position_ids, use_cache, past_key_value,
             self.q_proj, self.k_proj, self.v_proj, self.o_proj, 
-            self.head_dim, self.num_heads, self.num_key_value_heads
+            self.head_dim, num_heads, self.num_key_value_heads
         )
+
         if use_cache:
             o, pkv = ret
         else:
