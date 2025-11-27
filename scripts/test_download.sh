36a0d3d02d26467d8f894830ad37be28fc67dbe1
Nithid Anchaleenukoon <nithidan@mit.edu>
2025-11-27 17:52:47 -0500

    download longbench only

--- scripts/test_download.sh
+++ scripts/test_download.sh
@@ -1,7 +1,5 @@
 mkdir benchmark/data
-mkdir benchmark/data/infinite-bench
 mkdir benchmark/data/longbench
 
 python benchmark/download.py
 
-curl https://huggingface.co/datasets/xinrongzhang2022/InfiniteBench/resolve/main/kv_retrieval.jsonl
